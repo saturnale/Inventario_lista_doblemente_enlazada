df9a78f87d2da0f4ae6fa74058be968694cade7c
Ale Hernandez <115502854+saturnale@users.noreply.github.com>
2025-10-24 23:08:02 -0600

    Create producto.js

---
+++ producto.js
@@ -0,0 +1,18 @@
+class Producto {
+    constructor(codigo, nombre, cantidad, costo) {
+        this.codigo = codigo; 
+        this.nombre = nombre;  
+        this.cantidad = cantidad;  
+        this.costo = costo;  
+    }
+    
+    info() {
+        
+        return "Código: " + this.codigo + ", Nombre: " + this.nombre + ", Cantidad: " + this.cantidad + ", Costo: " + this.costo;
+    }
+    
+    infoHtml() {
+        
+        return "Código: " + this.codigo + "<br>Nombre: " + this.nombre + "<br>Cantidad: " + this.cantidad + "<br>Costo: " + this.costo + "<br><br>";
+    }
+}
